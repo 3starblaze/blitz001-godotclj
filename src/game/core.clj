8bb7473383c52bcba9b228b9615cc43ddd21f1a3
3starblaze <3starblaze@protonmail.com>
2021-12-17 10:49:12 +0200

    Migrate the whole game

--- src/game/core.clj
+++ src/game/core.clj
@@ -4,8 +4,74 @@
    [godotclj.bindings.godot :as godot]
    [godotclj.callbacks :as callbacks :refer [defer listen]]))
 
-(defn main-ready [_]
-  (println "Game has started"))
+(def Input (->object "Input"))
+
+(def player-velocity 300) ;; px/s
+(def gravity 1000) ;; px/s^2
+(def jump-velocity 500)
+(def coyote-time 0.02)
+(def max-level-count 3)
+
+(def current-velocity (atom [0 0]))
+(def coyote-time-counter (atom 0))
+(def current-level (atom 1))
+
+(defn- get-x-direction []
+  (- (.getActionStrength Input "ui_right")
+     (.getActionStrength Input "ui_left")))
+
+(defn can-jump? [body]
+  (or (.isOnFloor body)
+      (< @coyote-time-counter coyote-time)))
+
+(defn set-current-velocity [x y]
+  (swap! current-velocity #(vector (or x (first %)) (or y (second %)))))
+
+(defn physics-process [level delta]
+  (let [player (.getNode level "Player")]
+    (set-current-velocity (* (get-x-direction) player-velocity) nil)
+    (if (.isOnFloor player)
+      (reset! coyote-time-counter 0)
+      (swap! coyote-time-counter + delta))
+
+    (set-current-velocity
+     nil
+     (if (and (.isActionJustPressed Input "ui_up")
+              (can-jump? player))
+       (- jump-velocity)
+       (+ (second @current-velocity)
+          (* gravity delta))))
+    (.moveAndSlide player (api/vec2 @current-velocity) (api/vec2 [0 -1]))))
+
+(defn get-root []
+  (.getRoot (.getMainLoop (->object "_Engine"))))
+
+(defn load-level [n]
+  (.changeScene (.getTree (get-root))
+                (format "res://scenes/levels/Level%s.tscn" n)))
+
+(defn on-start-button-pressed [this]
+  (load-level 1))
+
+(defn main-ready [this]
+  (println "Game has started")
+  (.connect (.getNode this "CenterContainer/StartButton")
+            "pressed"
+            this
+            "_onStartButtonPressed"))
+
+(defn on-finish-flag-area-entered [_ _]
+  (println "Level has been completed!")
+  (if (<= (inc @current-level) max-level-count)
+    (do (swap! current-level inc)
+        (load-level @current-level))
+    (println "All levels have been completed!")))
+
+(defn level-ready [this]
+  (.connect (.getNode this "FinishFlag")
+            "body_entered"
+            this
+            "_onFinishFlagAreaEntered"))
 
 (defn main-process [_ _]
   nil)
@@ -21,7 +87,15 @@
     :create     (fn [& args] (println :create))
     :destroy    (fn [& args] (println :destroy))
     :methods    {"_ready" (simplify-method main-ready)
-                 "_process" (simplify-method main-process)}}})
+                 "_process" (simplify-method main-process)
+                 "_onStartButtonPressed"
+                 (simplify-method on-start-button-pressed)}}
+   "BaseLevel"
+   {:base "Node2D"
+    :methods {"_ready" (simplify-method level-ready)
+              "_physics_process" (simplify-method physics-process)
+              "_onFinishFlagAreaEntered"
+              (simplify-method on-finish-flag-area-entered)}}})
 
 (defn register-methods [p-handle]
   (godot/register-classes p-handle classes)
